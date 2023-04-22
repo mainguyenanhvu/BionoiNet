f9778a9c3e66ba5bb5cd7fba1560042e6662a352
mainguyenanhvu <26211591+mainguyenanhvu@users.noreply.github.com>
2023-04-22 11:39:22 +0700

    Add packages for CUDA 9.2

--- dependency/install.sh
+++ dependency/install.sh
@@ -1,4 +1,7 @@
-conda env create -n bionoinet python=3.6.8
-conda install -c conda-forge opencv
+conda create -n bionoinet python=3.6.8
+pip install opencv-python
 conda install pytorch==1.2.0 torchvision==0.4.0 cudatoolkit=9.2 -c pytorch
 pip install biopandas
+pip install scikit-learn
+pip install scikit-image
+pip install matplotlib
